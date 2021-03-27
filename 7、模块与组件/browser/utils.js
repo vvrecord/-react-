2852814c3c3c3a8b2ff7ad7de3903c4bedbc0bc3
0xHigos <1172860590@qq.com>
2021-03-27 12:43:13 +0800

    第一天学习笔记

---
+++ 7、模块与组件/browser/utils.js
@@ -0,0 +1,14 @@
+export function dateFormat(date, format = "YYYY-MM-DD HH:mm:ss") {
+    const config = {
+      YYYY: date.getFullYear(),
+      MM: date.getMonth() + 1,
+      DD: date.getDate(),
+      HH: date.getHours(),
+      mm: date.getMinutes(),
+      ss: date.getSeconds()
+    };
+    for (const key in config) {
+      format = format.replace(key, config[key]);
+    }
+    return format;
+  }
